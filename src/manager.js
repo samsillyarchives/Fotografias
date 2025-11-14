833e1dac5841b19900dd664bdbac3f24c4ebffcf
samsillyarchives <sml9740@nyu.edu>
2025-11-13 20:19:43 -0500

    manager.js

--- src/manager.js
+++ src/manager.js
@@ -3,7 +3,7 @@ import { LoadingManager } from "three";
 export function manager(){
     const loadingManager = new LoadingManager()
     loadingManager.onLoad = function() {
-        console.log('loaded')
+        console.log('loaded!')
     }
     return loadingManager
 }
